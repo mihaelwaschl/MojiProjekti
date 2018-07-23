43056ef988f377f4e4779cc6adad36ccc14733cf
Mihael <mihael.waschl@gmail.com>
2018-07-23 16:15:55 +0200

    commit 2

--- Hangman.py
+++ Hangman.py
@@ -6,8 +6,10 @@ def read_file(file):
     readed_file.close()
     return text
 
-def greb_word():
-    list_of_items = read_file('besede.txt').split('\n')
-    print(list_of_items)
+def greb_random_word(list_items):
+    list_of_items = read_file(list_items).split('\n')
     return list_of_items[int(random.uniform(0, len(list_of_items)))]
 
+def start_game(word):
+    for i in range(len(word)):
+        letter = input().strip()
