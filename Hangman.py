ef30ce5733f520da30aab3ee056eed02fdc6f542
Mihael <mihael.waschl@gmail.com>
2018-07-23 13:05:03 +0200

    commit 1

--- Hangman.py
+++ Hangman.py
@@ -1,8 +1,13 @@
+import random
+
 def read_file(file):
     readed_file = open(file, 'r', encoding='UTF-8')
     text = readed_file.read()
     readed_file.close()
     return text
 
-def greb_word(text_file):
-    
+def greb_word():
+    list_of_items = read_file('besede.txt').split('\n')
+    print(list_of_items)
+    return list_of_items[int(random.uniform(0, len(list_of_items)))]
+
